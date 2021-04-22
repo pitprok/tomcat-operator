687095bf6193a03f080f1f5aa9b2cdf87cc4024a
Petros Marios Prokopiou <pprokopi@redhat.com>
2021-04-22 21:36:21 +0300

    Initial draft of the Tomcat resource

--- api/v1alpha1/tomcat_types.go
+++ api/v1alpha1/tomcat_types.go
@@ -20,6 +20,16 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	// PodStateActive represents PodStatus.State when pod is active to serve requests
+	// it's connected in the Service load balancer
+	PodStateActive = "ACTIVE"
+	// PodStatePending represents PodStatus.State when pod is pending
+	PodStatePending = "PENDING"
+	// PodStateFailed represents PodStatus.State when pod has failed
+	PodStateFailed = "FAILED"
+)
+
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
@@ -28,14 +38,58 @@ type TomcatSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Tomcat. Edit tomcat_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// The base for the names of the deployed application resources
+	// +kubebuilder:validation:Pattern=^[a-z]([-a-z0-9]*[a-z0-9])?$
+	ApplicationName string `json:"applicationName"`
+	// The desired number of replicas for the application
+	// +kubebuilder:validation:Minimum=0
+	Replicas int32 `json:"replicas"`
+	// (Deployment method 1) Application image
+	TomcatImage *TomcatImageSpec `json:"tomcatImage,omitempty"`
+}
+
+// (Deployment method 1) Application image
+type TomcatImageSpec struct {
+	// The name of the application image to be deployed
+	ApplicationImage string `json:"applicationImage"`
+	// Pod health checks information
+	TomcatHealthCheck *TomcatHealthCheckSpec `json:"TomcatHealthCheck,omitempty"`
+
+}
+
+type TomcatHealthCheckSpec struct {
+	// String for the pod readiness health check logic
+	ServerReadinessScript string `json:"serverReadinessScript"`
+	// String for the pod liveness health check logic
+	ServerLivenessScript string `json:"serverLivenessScript,omitempty"`
 }
 
 // TomcatStatus defines the observed state of Tomcat
+//+kubebuilder:subresource:status
 type TomcatStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// Replicas is the actual number of replicas for the application
+	Replicas int32 `json:"replicas"`
+	// +listType=atomic
+	Pods []PodStatus `json:"pods,omitempty"`
+	// +listType=set
+	Hosts []string `json:"hosts,omitempty"`
+	// Represents the number of pods which are in scaledown process
+	// what particular pod is scaling down can be verified by PodStatus
+	//
+	// Read-only.
+	ScalingdownPods int32 `json:"scalingdownPods"`
+}
+
+// PodStatus defines the observed state of pods running the Tomcat application
+// +k8s:openapi-gen=true
+type PodStatus struct {
+	Name  string `json:"name"`
+	PodIP string `json:"podIP"`
+	// Represent the state of the Pod, it is used especially during scale down.
+	// +kubebuilder:validation:Enum=ACTIVE;PENDING;FAILED
+	State string `json:"state"`
 }
 
 //+kubebuilder:object:root=true
